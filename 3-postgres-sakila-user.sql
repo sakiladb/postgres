8b12f80e8d34d737cb6378cbf7f3ae48b9b9115d
neilotoole <neilotoole@apache.org>
2023-08-26 14:21:10 -0600

    The root user is now 'sakila'

--- 3-postgres-sakila-user.sql
+++ 3-postgres-sakila-user.sql
@@ -1,4 +1,9 @@
-CREATE USER sakila WITH PASSWORD 'p_ssW0rd' CREATEDB;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO sakila;
+-- The database dump used the "postgres" user. But we want everything to
+-- be owned by the sakila user.
+ALTER SCHEMA public OWNER TO sakila;
+ALTER DATABASE sakila OWNER TO sakila;
+REASSIGN OWNED BY postgres TO sakila;
+DROP OWNED BY postgres;
+DROP USER IF EXISTS postgres;
 
 SELECT 'sakiladb/postgres has successfully initialized.' AS sakiladb_completion_message;
